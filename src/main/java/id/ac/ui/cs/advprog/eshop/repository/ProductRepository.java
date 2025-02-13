a2e34e3c1fccd25a1204e284fbf3b4f0bfe1b75d
fachhhh <fachhhhs.01@gmail.com>
2025-02-13 22:14:15 +0700

    feat: implement UUID in ProductRepository for product creation

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -13,6 +13,7 @@ public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
     }
