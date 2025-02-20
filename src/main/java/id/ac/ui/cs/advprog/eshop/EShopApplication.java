f780d56ebab11054cb791049508644a5f7711f44
fachhhh <fachhhhs.01@gmail.com>
2025-02-21 01:50:05 +0700

    refactor: add private constructor to prevent instantiation in EShopApplication

--- src/main/java/id/ac/ui/cs/advprog/eshop/EShopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EShopApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EShopApplication {
 
+    private EShopApplication() {
+        // Prevent instantiation
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EShopApplication.class, args);
     }
-
 }
