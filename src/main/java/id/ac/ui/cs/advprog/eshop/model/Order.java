859612f5086054e7a4a297a29c3936bd4e04a0a3
fachhhh <fachhhhs.01@gmail.com>
2025-03-04 21:59:19 +0700

    feat: add order controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -11,44 +11,4 @@ public class Order {
     private long orderTime;
     private String author;
     private String status;
-
-    /*
-    private static List<String> VALID_STATUS = Arrays.asList("WAITING_PAYMENT", "FAILED", "CANCELLED", "SUCCESS");
-
-    public Order(List<Product> products, String author) {
-        if (products == null || products.isEmpty()) {
-            throw new IllegalArgumentException("Order must contain at least one product");
-        }
-        this.products = products;
-        this.author = author;
-        this.orderTime = System.currentTimeMillis();
-        this.status = "WAITING_PAYMENT";
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setProducts(List<Product> products) {
-        if (products == null || products.isEmpty()) {
-            throw new IllegalArgumentException("Order must contain at least one product");
-        }
-        this.products = products;
-    }
-
-    public void setOrderTime(long orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setStatus(String status) {
-        if (status == null || !VALID_STATUS.contains(status)) {
-            return;
-        }
-        this.status = status;
-    }
-     */
 }
