b4948235be52b6c26ef87ea200ebe7ec9582ce76
fachhhh <fachhhhs.01@gmail.com>
2025-03-07 17:41:57 +0700

    [REFACTOR]: improve payment repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,18 +1,44 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class PaymentRepository {
-    private final Map<String, Payment> payments = new HashMap<>();
+    private final List<Payment> paymentData = new ArrayList<>();
 
     public void save(Payment payment) {
-        payments.put(payment.getId(), payment);
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.set(i, payment);
+                return;
+            }
+            i++;
+        }
+
+        paymentData.add(payment);
     }
 
-    public Optional<Payment> findById(String id) {
-        return Optional.ofNullable(payments.get(id));
+    public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
+    }
+
+    public List<Payment> findAllByMethod(String method) {
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getMethod().equalsIgnoreCase(method)) {
+                result.add(savedPayment);
+            }
+        }
+        return result;
     }
 }
